39a791792b68a7125ce0bb37a0c2eedfc390f8dd
Emir Al <mythia@windowslive.com>
2022-02-02 12:17:23 +0300

    chore: add app colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          50: '#F5FBFD',
+          200: '#E8EEF0',
+          800: '#3D4143',
+          900: '#1C2022',
+        },
+        redbull: '#0800EF',
+        mercedes: '#00D2BD',
+        mclaren: '#FF9900',
+        ferrari: '#DC0000',
+        alphatauri: '#2B4562',
+        astonmartin: '#006F62',
+        alpine: '#0091FF',
+        alfaromeo: '#900000',
+        williams: '#0059FF',
+        haas: {
+          50: '#FFFFFF',
+          900: '#3C3C3C',
+        },
+      },
+    },
   },
   plugins: [],
 };
