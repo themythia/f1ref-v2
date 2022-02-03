3a1bfe77174ced1d81ce4beb9b48142bb5a181c5
Emir Al <mythia@windowslive.com>
2022-02-03 13:41:32 +0300

    add: basic ui for navbar menu

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,6 +1,21 @@
 const Menu = () => {
   return (
-    <div className='bg-bg-50 dark:bg-bg-800 w-screen h-[calc(100vh-56px)] mt-14'></div>
+    <div className='bg-bg-50 dark:bg-bg-800 w-screen h-[calc(100vh-56px)] mt-14 px-4'>
+      <ul className='font-poppins text-lg pt-4 pb-2 border-b border-bg-300'>
+        <li className='mb-2'>Home</li>
+        <li className='mb-2'>Schedule</li>
+        <li className='mb-2'>Drivers</li>
+        <li className='mb-2'>Constructors</li>
+      </ul>
+      <ul className='font-poppins text-lg pt-4'>
+        <li className='mb-2'>
+          <span>Dark Theme</span>
+        </li>
+        <li className='mb-2'>
+          <span>Measurement Unit</span>
+        </li>
+      </ul>
+    </div>
   );
 };
 export default Menu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdClose, MdMenu } from 'react-icons/md';
+import { toggleMenu } from '../../slices/settingsSlice';
+import Menu from './Menu';
 
 const Navbar = () => {
-  const [menuState, setMenuState] = useState(false);
-
-  const toggleMenu = () => setMenuState(!menuState);
+  const showMenu = useSelector((store) => store.settings.showMenu);
+  const dispatch = useDispatch();
 
   return (
-    <nav className='fixed flex items-center justify-center h-14 w-full bg-bg-50 dark:bg-bg-800 px-4 shadow-2px'>
-      <button onClick={toggleMenu} className='absolute left-4'>
-        {menuState ? <MdClose size={32} /> : <MdMenu size={32} />}
-      </button>
-      <p className='font-kronaOne text-2xl text-center'>
-        F1<span className='text-mercedes'>/</span>REF
-      </p>
-    </nav>
+    <Fragment>
+      <nav className='fixed flex items-center justify-center h-14 w-full bg-bg-50 dark:bg-bg-800 px-4 shadow-2px top-0'>
+        <button
+          onClick={() => dispatch(toggleMenu())}
+          className='absolute left-4'
+        >
+          {showMenu ? <MdClose size={32} /> : <MdMenu size={32} />}
+        </button>
+        <p className='font-kronaOne text-2xl text-center'>
+          F1<span className='text-mercedes'>/</span>REF
+        </p>
+      </nav>
+      {showMenu && <Menu />}
+    </Fragment>
   );
 };
 export default Navbar;
