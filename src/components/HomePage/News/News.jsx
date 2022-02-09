78e0675ce10df01993f55a08b462e6a364e797ae
Emir Al <mythia@windowslive.com>
2022-02-09 17:20:07 +0300

    fix: margin of News title

--- src/components/HomePage/News/News.jsx
+++ src/components/HomePage/News/News.jsx
@@ -15,7 +15,9 @@ const News = () => {
 
   return (
     <Container>
-      <p className='font-poppins text-lg text-bg-800 dark:text-bg-50'>News:</p>
+      <p className='font-poppins text-lg text-bg-800 dark:text-bg-50 mb-4'>
+        News:
+      </p>
       <div className='flex flex-col gap-y-2'>
         {newsArray.map((news, index) => (
           <NewsItem key={index} news={news} />
