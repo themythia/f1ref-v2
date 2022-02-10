5a3c37fed51c05e6873bb4243a75124ecfa88315
Emir Al <mythia@windowslive.com>
2022-02-10 12:44:16 +0300

    add: new styling for NewsItem component

--- src/components/HomePage/News/News.jsx
+++ src/components/HomePage/News/News.jsx
@@ -4,13 +4,20 @@ import { addNews } from '../../../slices/newsSlice';
 import Container from '../Container';
 import scrapeNews from '../../../utils/scrapeNews';
 import NewsItem from './NewsItem';
+import NewsItemSkeleton from './NewsItemSkeleton';
 
 const News = () => {
   const dispatch = useDispatch();
   const newsArray = useSelector((store) => store.news);
+  console.log('newsArray:', newsArray);
+  const [loading, setLoading] = useState(true);
+  console.log('loading:', loading);
 
   useEffect(() => {
-    scrapeNews().then((data) => dispatch(addNews(data)));
+    scrapeNews().then((data) => {
+      dispatch(addNews(data));
+      setLoading(false);
+    });
   }, [dispatch]);
 
   return (
@@ -19,9 +26,12 @@ const News = () => {
         News:
       </p>
       <div className='flex flex-col gap-y-2'>
-        {newsArray.map((news, index) => (
-          <NewsItem key={index} news={news} />
-        ))}
+        {loading &&
+          [0, 1, 2, 3, 4].map((item, index) => (
+            <NewsItemSkeleton key={index} />
+          ))}
+        {!loading &&
+          newsArray.map((news, index) => <NewsItem key={index} news={news} />)}
       </div>
     </Container>
   );
--- src/components/HomePage/News/NewsItem.jsx
+++ src/components/HomePage/News/NewsItem.jsx
@@ -3,12 +3,20 @@ const NewsItem = ({ news }) => {
 
   return (
     <a href={link} target='_blank' rel='noreferrer'>
-      <div className='flex flex-col rounded bg-bg-200 dark:bg-bg-900 max-w-full shadow-2px relative'>
+      {/* <div className='flex flex-col rounded bg-bg-200 dark:bg-bg-900 max-w-full shadow-2px relative'>
         <img src={image} alt={description} className='rounded' />
-        <div className='w-full absolute bottom-0 bg-gradient-to-t from-bg-800'>
+        <div className='w-full rounded absolute bottom-0 bg-gradient-to-t from-bg-800'>
           <p className='p-2 font-openSans font-semibold text-sm text-bg-50 drop-shadow hover:underline'>
             {title}
           </p>
+        </div>
+      </div> */}
+      <div className='flex flex-row items-center w-full rounded shadow-2px bg-bg-200 dark:bg-bg-900'>
+        <div className='basis-2/5'>
+          <img src={image} alt={description} className='rounded shadow-2px' />
+        </div>
+        <div className='basis-3/5 font-openSans font-semibold text-sm p-2 max-h-full h-full self-start'>
+          <span className='line-clamp-3 '>{title}</span>
         </div>
       </div>
     </a>
