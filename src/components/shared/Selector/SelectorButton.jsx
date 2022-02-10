50b873842ad41386f0718406e7a066f32f291b4a
Emir Al <mythia@windowslive.com>
2022-02-10 16:29:17 +0300

    add: hover animations for SelectorButton component

--- src/components/shared/Selector/SelectorButton.jsx
+++ src/components/shared/Selector/SelectorButton.jsx
@@ -4,7 +4,7 @@ const SelectorButton = (props) => {
       className={`py-1 px-2 font-openSans text-sm border rounded-full border-bg-800 dark:border-bg-50 h-7 flex items-center ${
         props.active
           ? 'bg-bg-800 text-bg-50 dark:bg-bg-50 dark:text-bg-800'
-          : 'bg-bg-50 text-bg-800 dark:bg-bg-800 dark:text-bg-50'
+          : 'bg-bg-50 hover:bg-bg-200 duration-200 text-bg-800 dark:bg-bg-800 hover:dark:bg-bg-900 dark:text-bg-50 hover:scale-105 active:scale-100'
       }`}
       onClick={() => props.setActive(props.index)}
     >
