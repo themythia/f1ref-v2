7263207260fdc942f8360f85e5d8624044387631
Emir Al <mythia@windowslive.com>
2022-02-11 12:44:18 +0300

    fix: change html tags with semantic versions

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='flex flex-col gap-y-2 w-screen p-4 md:p-8 lg:px-48 lg:py-6 bg-bg-50 dark:bg-bg-800 shadow-2px font-openSans text-xs md:text-sm'>
+    <footer className='flex flex-col gap-y-2 w-screen p-4 md:p-8 lg:px-48 lg:py-6 bg-bg-50 dark:bg-bg-800 shadow-2px font-openSans text-xs md:text-sm'>
       <p>Made with ❤️ for personal use.</p>
       <p>
         This website is unofficial and is not associated in any way with the
@@ -8,7 +8,7 @@ const Footer = () => {
         ONE WORLD CHAMPIONSHIP, GRAND PRIX and related marks are trade marks of
         Formula One Licensing B.V.
       </p>
-    </div>
+    </footer>
   );
 };
 export default Footer;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,14 +5,14 @@ import Standings from './Standings/Standings';
 
 const HomePage = () => {
   return (
-    <div className='mt-14 p-4 md:p-8 lg:px-48 lg:py-6  flex flex-col gap-y-4'>
+    <main className='mt-14 p-4 md:p-8 lg:px-48 lg:py-6  flex flex-col gap-y-4'>
       <News />
       <div className='flex flex-col gap-y-4 md:flex md:flex-row md:gap-x-4'>
         <NextRace />
         <LastRace />
       </div>
       <Standings />
-    </div>
+    </main>
   );
 };
 export default HomePage;
