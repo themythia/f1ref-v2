de4cdf71257b5866f95bbb96f44bddb05507577a
Emir Al <mythia@windowslive.com>
2022-02-11 18:27:35 +0300

    add: support nationality to code conversion

--- src/utils/countryCodes.js
+++ src/utils/countryCodes.js
@@ -1,8 +1,14 @@
 import countries from 'i18n-iso-countries';
+import nationalities from 'i18n-nationality';
 countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+nationalities.registerLocale(require('i18n-nationality/langs/en.json'));
 
-const countryCodes = (country) => {
-  if (country === 'UAE') return 'AE';
-  else return countries.getAlpha2Code(country, 'en');
+const countryCodes = (country, type = 'country') => {
+  if (type === 'country') {
+    if (country === 'UAE') return 'AE';
+    else return countries.getAlpha2Code(country, 'en');
+  } else if (type === 'nationality') {
+    return nationalities.getAlpha2Code(country, 'en');
+  }
 };
 export default countryCodes;
