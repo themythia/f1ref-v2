05f8d49995c3b7e4d7c4ea9d61b4191b038780af
Emir Al <mythia@windowslive.com>
2022-02-15 14:11:53 +0300

    styling: change layout to grid

--- src/components/CalendarPage/CalendarItem.jsx
+++ src/components/CalendarPage/CalendarItem.jsx
@@ -1,6 +1,5 @@
 import Flag from '../shared/Flag';
 import { useNavigate } from 'react-router-dom';
-import { ImCircleRight } from 'react-icons/im';
 
 const CalendarItem = ({ race }) => {
   const navigate = useNavigate();
@@ -14,7 +13,7 @@ const CalendarItem = ({ race }) => {
 
   return (
     <div
-      className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px p-4 lg:p-6 max-w-full md:w-[calc(50%-8px)] cursor-pointer hover:scale-[1.02] hover:shadow-8px duration-200 group'
+      className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px p-4 md:p-6 w-full cursor-pointer hover:scale-[1.02] hover:shadow-8px duration-200 group col-span-4 sm:col-span-4 md:col-span-6 xl:col-span-4'
       onClick={() => navigate(`/calendar/${race.round}`)}
     >
       <div className='flex flex-row font-poppins text-sm justify-between'>
@@ -23,13 +22,12 @@ const CalendarItem = ({ race }) => {
           {event.toLocaleDateString('en-GB', options)}
         </span>
       </div>
-      <div className='flex flex-row gap-x-4 items-center mt-2 lg:mt-4'>
-        <Flag size='36' country={race.countryCode} />
+      <div className='flex flex-row gap-x-4 items-center mt-2 md:mt-3'>
+        <Flag size='26' country={race.countryCode} />
         <div className='flex flex-col font-openSans'>
           <span className='text-sm font-semibold'>{race.raceName}</span>
           <span className='text-xs'>{race.circuitName}</span>
         </div>
-        <ImCircleRight size={24} className='hidden group-hover:block ml-auto' />
       </div>
     </div>
   );
--- src/components/CalendarPage/CalendarItemSkeleton.jsx
+++ src/components/CalendarPage/CalendarItemSkeleton.jsx
@@ -1,11 +1,11 @@
 const CalendarItemSkeleton = () => {
   return (
-    <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px p-4 lg:p-6 max-w-full md:w-[calc(50%-8px)] h-24 lg:h-[120px] flex flex-col justify-center'>
+    <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px p-4 md:p-6 w-full cursor-pointer hover:scale-[1.02] hover:shadow-8px duration-200 group col-span-4 sm:col-span-4 md:col-span-6 xl:col-span-4 h-24 lg:h-[120px] flex flex-col justify-center'>
       <div className='flex flex-row justify-between animate-pulse-tw'>
         <div className='bg-bg-600 w-1/5 h-[14px] rounded-full'></div>
         <div className='bg-bg-600 w-2/5 h-[14px] rounded-full'></div>
       </div>
-      <div className='flex flex-row gap-x-4 mt-2 lg:mt-4 w-full animate-pulse-tw'>
+      <div className='flex flex-row gap-x-4 mt-2 md:mt-3 w-full animate-pulse-tw'>
         <div className='bg-bg-600 h-9 w-9 rounded-full shadow-4px'></div>
         <div className='flex flex-col w-4/5 justify-center'>
           <div className='bg-bg-600 h-[14px] w-3/5 rounded-full'></div>
