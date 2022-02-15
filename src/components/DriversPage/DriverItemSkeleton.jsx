7286fe5dfe44930d05d1f2f47a9e3faee94757d2
Emir Al <mythia@windowslive.com>
2022-02-15 16:55:30 +0300

    style: fix DriverItemSkeleton

--- src/components/DriversPage/DriverItemSkeleton.jsx
+++ src/components/DriversPage/DriverItemSkeleton.jsx
@@ -1,14 +1,14 @@
 const DriverItemSkeleton = () => {
   return (
-    <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px max-w-full md:w-[calc((100%-16px)/2)] lg:w-[calc(50%-12px)] xl:w-[calc((100%-48px)/3)] 2xl:w-[calc((100%-72px)/4)] cursor-pointer hover:scale-[1.02] hover:shadow-8px duration-200 group flex flex-col items-center'>
-      <div className='w-full aspect-square bg-bg-600 animate-pulse-tw rounded-t'></div>
+    <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px w-full cursor-pointer hover:scale-[1.02] duration-200 group col-span-4 sm:col-span-4 md:col-span-4 flex flex-row items-center h-[84px] group'>
+      <div className='h-full aspect-square bg-bg-600 shadow-2px animate-pulse-tw rounded-l'></div>
       <div className='flex flex-row w-full p-4 lg:p-6 justify-between animate-pulse-tw'>
-        <div className='h-9 w-9 aspect-square rounded-full bg-bg-600'></div>
-        <div className='flex flex-col w-full items-center '>
+        <div className='h-full aspect-square rounded-full bg-bg-600'></div>
+        <div className='flex flex-col w-full '>
           <div className='h-[14px] w-3/5 bg-bg-600 rounded-full mb-[3px]'></div>
           <div className='h-3 w-2/5 bg-bg-600 rounded-full'></div>
         </div>
-        <div className='h-9 w-9 aspect-square rounded-full bg-bg-600'></div>
+        <div className='w-5 h-5 aspect-square rounded-full self-center bg-bg-600'></div>
       </div>
     </div>
   );
