e19ded88d8f431f3269f0872a30c55b0f4de1b60
Emir Al <mythia@windowslive.com>
2022-03-04 11:39:55 +0300

    add: logic for DSQ & NC status for race result

--- src/components/RacePage/RaceResultItem.jsx
+++ src/components/RacePage/RaceResultItem.jsx
@@ -1,18 +1,23 @@
 import TeamBar from '../HomePage/Standings/TeamBar';
 
 const RaceResultItem = ({ driver }) => {
+  const status = /(^\+)|(^\d)/gi.test(driver.time) ? driver.time : 'DNF';
+  const position =
+    driver.positionText === 'D'
+      ? 'DSQ'
+      : driver.positionText === 'R'
+      ? 'NC'
+      : driver.positionText;
   return (
     <div className='flex flex-row rounded shadow-2px bg-bg-200 dark:bg-bg-900 p-2 text-sm items-center'>
-      <span className='font-poppins text-lg w-6 text-center'>
-        {driver.positionText}
-      </span>
+      <span className='font-poppins text-lg w-6 text-center'>{position}</span>
       <div className='flex flex-row'>
         <TeamBar team={driver.teamCode} />
         <span className='font-openSans font-semibold text-left'>
           {driver.name}
         </span>
       </div>
-      <span className='ml-auto text-right'>{driver.time}</span>
+      <span className='ml-auto text-right'>{status}</span>
     </div>
   );
 };
