9285da437b2a44640355052ebccd3d22371a2414
Emir Al <mythia@windowslive.com>
2022-03-09 11:59:19 +0300

    style: improve spacing on bigger screens

--- src/components/shared/Selector/Selector.jsx
+++ src/components/shared/Selector/Selector.jsx
@@ -15,7 +15,7 @@ const Selector = (props) => {
 
   return (
     <div className='max-w-full overflow-auto'>
-      <div className='py-2 my-2 overflow-hidden flex gap-x-2 justify-center min-w-fit'>
+      <div className='py-2 my-2 md:py-3 md:my-3 overflow-hidden flex gap-x-2 justify-center min-w-fit'>
         {labels[props.type].map((session, index) => (
           <SelectorButton
             key={index}
