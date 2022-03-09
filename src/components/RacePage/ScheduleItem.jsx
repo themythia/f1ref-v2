7c7e998482763bcd7ad7b45e63c44551ac200f8a
Emir Al <mythia@windowslive.com>
2022-03-09 11:58:52 +0300

    add: handleSessionTime function

--- src/components/RacePage/ScheduleItem.jsx
+++ src/components/RacePage/ScheduleItem.jsx
@@ -1,16 +1,23 @@
 const ScheduleItem = ({ session, time }) => {
-  const event = new Date(time);
-  const options = {
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
+  const handleSesssionTime = () => {
+    if (time === 'TBC') return 'TBC';
+    else {
+      const event = new Date(time);
+      const options = {
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      };
+
+      return event.toLocaleDateString('en-GB', options);
+    }
   };
 
   return (
     <div className='flex flex-row justify-between rounded shadow-2px bg-bg-200 dark:bg-bg-900 p-2 font-openSans text-sm'>
       <span className='font-semibold text-left'>{session}</span>
-      <span className=''>{event.toLocaleDateString('en-GB', options)}</span>
+      <span className=''>{handleSesssionTime()}</span>
     </div>
   );
 };
