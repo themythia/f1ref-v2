a2ca1f61be6154c7e919242953e571ad8ccc5925
Emir Al <mythia@windowslive.com>
2022-03-11 10:58:02 +0300

    add: addDriverStats reducer

--- src/slices/driversSlice.js
+++ src/slices/driversSlice.js
@@ -8,8 +8,31 @@ export const driversSlice = createSlice({
       state = action.payload;
       return state;
     },
+    addDriverStats: (state, action) => {
+      const index = state.findIndex(
+        (driver) => driver.id === action.payload.id
+      );
+
+      const champ =
+        action.payload.id === 'hamilton'
+          ? 7
+          : action.payload.id === 'vettel'
+          ? 4
+          : action.payload.id === 'max_verstappen'
+          ? 1
+          : action.payload.id === 'alonso'
+          ? 2
+          : action.payload.id === 'raikkonen'
+          ? 1
+          : 0;
+
+      state[index].stats = {
+        ...action.payload.stats,
+        champ,
+      };
+    },
   },
 });
 
-export const { addDrivers } = driversSlice.actions;
+export const { addDrivers, addDriverStats } = driversSlice.actions;
 export default driversSlice.reducer;
