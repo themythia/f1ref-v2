ec07796192798e9c215de43fa4857c5b164f6704
Emir Al <mythia@windowslive.com>
2022-03-14 12:57:02 +0300

    refactor: Selector & SelectorButton to use redux state

--- src/components/shared/Selector/Selector.jsx
+++ src/components/shared/Selector/Selector.jsx
@@ -1,9 +1,6 @@
 import SelectorButton from './SelectorButton';
-import { useState } from 'react';
 
 const Selector = (props) => {
-  const [activeButton, setActiveButton] = useState(0);
-
   return (
     <div className='max-w-full overflow-auto'>
       <div className='py-2 my-2 md:py-3 md:my-3 overflow-hidden flex gap-x-2 justify-center min-w-fit'>
@@ -11,9 +8,8 @@ const Selector = (props) => {
           <SelectorButton
             key={index}
             text={session}
-            active={props.active === index}
-            setActive={props.setActive}
             index={index}
+            type={props.type}
           />
         ))}
       </div>
--- src/components/shared/Selector/SelectorButton.jsx
+++ src/components/shared/Selector/SelectorButton.jsx
@@ -1,14 +1,27 @@
-const SelectorButton = (props) => {
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelector } from '../../../slices/settingsSlice';
+
+const SelectorButton = ({ type, index, text }) => {
+  const dispatch = useDispatch();
+  const selectorState = useSelector((store) => store.settings.selector[type]);
+  const [active, setActive] = useState(selectorState === index ? true : false);
+
+  useEffect(() => {
+    if (selectorState === index) setActive(true);
+    else setActive(false);
+  }, [selectorState, index]);
+
   return (
     <button
       className={`py-1 px-2 font-openSans text-sm border rounded-full border-bg-800 dark:border-bg-50 h-7 flex items-center ${
-        props.active
+        active
           ? 'bg-bg-800 text-bg-50 dark:bg-bg-50 dark:text-bg-800'
           : 'bg-bg-50 hover:bg-bg-200 duration-200 text-bg-800 dark:bg-bg-800 hover:dark:bg-bg-900 dark:text-bg-50 hover:scale-105 active:scale-100'
       }`}
-      onClick={() => props.setActive(props.index)}
+      onClick={() => dispatch(setSelector({ type, index }))}
     >
-      {props.text}
+      {text}
     </button>
   );
 };
