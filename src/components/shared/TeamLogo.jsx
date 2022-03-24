eeac615441e2862949c3d29e1540cc8ec462c6f8
Emir Al <mythia@windowslive.com>
2022-03-24 16:02:57 +0300

    add: ability to switch between logo types

--- src/components/shared/TeamLogo.jsx
+++ src/components/shared/TeamLogo.jsx
@@ -1,35 +1,70 @@
-import alfa from '../../assets/teamlogos/alfa_romeo.svg';
-import alphatauri from '../../assets/teamlogos/alphatauri.svg';
-import alpine from '../../assets/teamlogos/alpine.svg';
-import aston_martin from '../../assets/teamlogos/astonmartin.svg';
-import ferrari from '../../assets/teamlogos/ferrari.svg';
-import haas from '../../assets/teamlogos/haas.svg';
-import mclaren from '../../assets/teamlogos/mclaren.svg';
-import mercedes from '../../assets/teamlogos/mercedes.svg';
-import red_bull from '../../assets/teamlogos/redbull.svg';
-import williams from '../../assets/teamlogos/williams.svg';
+import alfa from '../../assets/teamlogos/round/alfa_romeo.svg';
+import alphatauri from '../../assets/teamlogos/round/alphatauri.svg';
+import alpine from '../../assets/teamlogos/round/alpine.svg';
+import aston_martin from '../../assets/teamlogos/round/astonmartin.svg';
+import ferrari from '../../assets/teamlogos/round/ferrari.svg';
+import haas from '../../assets/teamlogos/round/haas.svg';
+import mclaren from '../../assets/teamlogos/round/mclaren.svg';
+import mercedes from '../../assets/teamlogos/round/mercedes.svg';
+import red_bull from '../../assets/teamlogos/round/redbull.svg';
+import williams from '../../assets/teamlogos/round/williams.svg';
+import alfaBig from '../../assets/teamlogos/big/alfa_romeo.svg';
+import alphatauriBig from '../../assets/teamlogos/big/alphatauri.svg';
+import alpineBig from '../../assets/teamlogos/big/alpine.svg';
+import aston_martinBig from '../../assets/teamlogos/big/astonmartin.svg';
+import ferrariBig from '../../assets/teamlogos/big/ferrari.svg';
+import haasBig from '../../assets/teamlogos/big/haas.svg';
+import mclarenBig from '../../assets/teamlogos/big/mclaren.svg';
+import mercedesBig from '../../assets/teamlogos/big/mercedes.svg';
+import red_bullBig from '../../assets/teamlogos/big/redbull.svg';
+import williamsBig from '../../assets/teamlogos/big/williams.svg';
 
-const TeamLogo = ({ team }) => {
+const TeamLogo = ({ team, type }) => {
   const logo = {
-    alfa,
-    alphatauri,
-    alpine,
-    aston_martin,
-    ferrari,
-    haas,
-    mclaren,
-    mercedes,
-    red_bull,
-    williams,
+    round: {
+      alfa,
+      alphatauri,
+      alpine,
+      aston_martin,
+      ferrari,
+      haas,
+      mclaren,
+      mercedes,
+      red_bull,
+      williams,
+    },
+    big: {
+      alfa: alfaBig,
+      alphatauri: alphatauriBig,
+      alpine: alpineBig,
+      'aston-martin': aston_martinBig,
+      ferrari: ferrariBig,
+      haas: haasBig,
+      mclaren: mclarenBig,
+      mercedes: mercedesBig,
+      'red-bull': red_bullBig,
+      williams: williamsBig,
+    },
   };
 
-  return (
-    <img
-      src={logo[team]}
-      alt={team}
-      className='max-h-[84px] w-full'
-      loading='lazy'
-    />
-  );
+  if (type === 'round') {
+    return (
+      <img
+        src={logo.round[team]}
+        alt={`Logo of ${team}`}
+        className='max-h-[84px] w-full'
+        loading='lazy'
+      />
+    );
+  } else if (type === 'big') {
+    return (
+      <img
+        src={logo.big[team]}
+        alt={`Logo of ${team}`}
+        loading='lazy'
+        className='w-full aspect-16/9'
+      />
+    );
+  }
 };
 export default TeamLogo;
