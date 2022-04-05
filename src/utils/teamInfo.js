8337a908cbcdd24665331d4f1ba2baba266e397e
Emir Al <mythia@windowslive.com>
2022-04-05 12:17:55 +0300

    add: teamInfo function

---
+++ src/utils/teamInfo.js
@@ -0,0 +1,74 @@
+const teamInfo = {
+  alfa: {
+    fullName: 'Alfa Romeo F1 Team ORLEN',
+    base: 'Hinwil, Switzerland',
+    teamChief: 'Frédéric Vasseur',
+    chassis: 'C42',
+    powerUnit: 'Ferrari',
+  },
+  alphatauri: {
+    fullName: 'Scuderia AlphaTauri',
+    base: 'Faenza, Italy',
+    teamChief: 'Franz Tost',
+    chassis: 'AT03',
+    powerUnit: 'Red Bull Powertrains',
+  },
+  alpine: {
+    fullName: 'BWT Alpine F1 Team',
+    base: 'Enstone, United Kingdom',
+    teamChief: 'Otmar Szafnauer',
+    chassis: 'A522',
+    powerUnit: 'Renault',
+  },
+  aston_martin: {
+    fullName: 'Aston Martin Aramco Cognizant F1 Team',
+    base: 'Silverstone, United Kingdom',
+    teamChief: 'Mike Krack',
+    chassis: 'AMR22',
+    powerUnit: 'Mercedes',
+  },
+  ferrari: {
+    fullName: 'Scuderia Ferrari',
+    base: 'Marenello, Italy',
+    teamChief: 'Mattia Binotto',
+    chassis: 'F1-75',
+    powerUnit: 'Ferrari',
+  },
+  mclaren: {
+    fullName: 'McLaren F1 Team',
+    base: 'Woking, United Kingdom',
+    teamChief: 'Andreas Seidl',
+    chassis: 'MCL36',
+    powerUnit: 'Mercedes',
+  },
+  mercedes: {
+    fullName: 'Mercedes-AMG Petronas F1 Team',
+    base: 'Brackley, United Kingdom',
+    teamChief: 'Toto Wolff',
+    chassis: 'W13',
+    powerUnit: 'Mercedes',
+  },
+  red_bull: {
+    fullName: 'Oracle Red Bull Racing',
+    base: 'Milton Keynes, United Kingdom',
+    teamChief: 'Christian Horner',
+    chassis: 'RB18',
+    powerUnit: 'Red Bull Powertrains',
+  },
+  williams: {
+    fullName: 'Williams Racing',
+    base: 'Grove, United Kingdom',
+    teamChief: 'Jost Capito',
+    chassis: 'FW44',
+    powerUnit: 'Mercedes',
+  },
+  haas: {
+    fullName: 'Haas F1 Team',
+    base: 'Kannapolis, United States',
+    teamChief: 'Guenther Steiner',
+    chassis: 'VF-22',
+    powerUnit: 'Ferrari',
+  },
+};
+
+export default teamInfo;
