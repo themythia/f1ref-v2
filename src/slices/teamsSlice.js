07a8ceff858f711025ee4bf8a487aec359239f9c
Emir Al <mythia@windowslive.com>
2022-04-05 11:41:59 +0300

    add: addTeamStats reducer

--- src/slices/teamsSlice.js
+++ src/slices/teamsSlice.js
@@ -8,8 +8,32 @@ export const teamsSlice = createSlice({
       state = action.payload;
       return state;
     },
+    addTeamStats: (state, action) => {
+      const index = state.findIndex((team) => team.id === action.payload.id);
+
+      const champ =
+        action.payload.id === 'ferrari'
+          ? 16
+          : action.payload.id === 'mercedes'
+          ? 8
+          : action.payload.id === 'red_bull'
+          ? 4
+          : action.payload.id === 'mclaren'
+          ? 8
+          : action.payload.id === 'williams'
+          ? 9
+          : 0;
+
+      state[index].stats = {
+        ...action.payload.stats,
+        career: {
+          ...action.payload.stats.career,
+          champ,
+        },
+      };
+    },
   },
 });
 
-export const { addTeams } = teamsSlice.actions;
+export const { addTeams, addTeamStats } = teamsSlice.actions;
 export default teamsSlice.reducer;
