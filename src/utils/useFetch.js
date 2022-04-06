a89cc09d2ef2c1bf0271add501c0763934432ff8
Emir Al <mythia@windowslive.com>
2022-04-06 15:11:37 +0300

    add: logic for checking if data fetched before

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const useFetch = (url, dependency, shapeFunc, options) => {
+const useFetch = (url, dependency, shapeFunc, isFetched) => {
   const [loading, setLoading] = useState(true);
   const [status, setStatus] = useState(false);
   const [response, setResponse] = useState(null);
@@ -9,18 +9,25 @@ const useFetch = (url, dependency, shapeFunc, options) => {
   useEffect(() => {
     const fetchApi = async () => {
       try {
-        const res = await fetch(url, options);
-        const data = await res.json();
+        if (!isFetched) {
+          const res = await fetch(url);
+          const data = await res.json();
 
-        // sets response state
-        // provided function shapes the response as needed
-        setResponse(shapeFunc(data));
-        setStatus(true);
-        setLoading(false);
+          // sets response state
+          // provided function shapes the response as needed
+          setResponse(shapeFunc(data));
+          setStatus(true);
+          setLoading(false);
+        }
       } catch (error) {
         setError(error);
         setStatus(false);
         setLoading(true);
+      } finally {
+        if (isFetched) {
+          setStatus(true);
+          setLoading(false);
+        }
       }
     };
 
