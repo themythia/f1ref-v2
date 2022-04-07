99ee1116f53c281bd81bd355e0300d50dd5a50e7
Emir Al <mythia@windowslive.com>
2022-04-07 13:26:32 +0300

    refactor: implement data fetching with new useFetch hook

--- src/components/HomePage/NextRace/NextRace.jsx
+++ src/components/HomePage/NextRace/NextRace.jsx
@@ -1,33 +1,38 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getNextRace } from '../../../utils/api';
 import { addNextRace } from '../../../slices/scheduleSlice';
 import Flag from '../../shared/Flag';
 import Selector from '../../shared/Selector/Selector';
 import Countdown from './Countdown';
 import circuitData from '../../../utils/circuitData';
 import SwitchTransitionWrapper from '../../shared/SwitchTransitionWrapper';
+import useFetch from '../../../utils/useFetch';
+import { shapeScheduleData } from '../../../utils/api/shapeScheduleData';
 
 const NextRace = () => {
   const nextRace = useSelector((store) => store.schedule.nextRace);
   const dispatch = useDispatch();
   const activeButton = useSelector((store) => store.settings.selector.nextRace);
-  const [loading, setLoading] = useState(true);
   const [activeSessionTime, setActiveSessionTime] = useState(null);
 
-  useEffect(() => {
-    getNextRace()
-      .then((data) => dispatch(addNextRace(data)))
-      .then(() => setLoading(false));
-  }, [dispatch]);
+  const { loading, status, response, error } = useFetch(
+    'https://ergast.com/api/f1/current/next.json',
+    [],
+    shapeScheduleData,
+    nextRace.length > 0 ? true : false
+  );
 
   useEffect(() => {
-    if (!loading) {
+    if (response) dispatch(addNextRace(response));
+  }, [dispatch, response]);
+
+  useEffect(() => {
+    if (Object.keys(nextRace).length > 0) {
       const raceSchedule = circuitData[nextRace.circuitId]?.schedule;
       const activeSession = Object.keys(raceSchedule)[activeButton];
       setActiveSessionTime(raceSchedule[activeSession]);
     }
-  }, [loading, activeButton, activeSessionTime, nextRace.circuitId]);
+  }, [activeButton, activeSessionTime, nextRace.circuitId, nextRace]);
 
   return (
     <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px p-4 md:p-6 w-full col-span-full sm:col-span-4 md:col-span-6 mb-4 md:mb-6'>
