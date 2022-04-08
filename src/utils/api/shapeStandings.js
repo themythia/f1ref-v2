26a68186211afa2d23e88724bb489dce52b86635
Emir Al <mythia@windowslive.com>
2022-04-08 12:23:44 +0300

    add: shapeStandings function to shape standings data

---
+++ src/utils/api/shapeStandings.js
@@ -0,0 +1,36 @@
+export const shapeStandings = (data) => {
+  let type =
+    data.MRData.url ===
+    'http://ergast.com/api/f1/current/constructorstandings.json'
+      ? 'constructor'
+      : 'driver';
+
+  const standingsData =
+    type === 'driver'
+      ? data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+      : data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+
+  const standings = standingsData.map((item) => {
+    const name =
+      type === 'driver'
+        ? item.Driver.givenName + ' ' + item.Driver.familyName
+        : item.Constructor.name === 'Alpine F1 Team'
+        ? 'Alpine'
+        : item.Constructor.name === 'Haas F1 Team'
+        ? 'Haas'
+        : item.Constructor.name;
+
+    const team =
+      type === 'driver'
+        ? item.Constructors[0].constructorId
+        : item.Constructor.constructorId;
+
+    return {
+      name,
+      position: item.position,
+      team,
+      points: item.points,
+    };
+  });
+  return standings;
+};
