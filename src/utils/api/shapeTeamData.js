fab71cc230bf97aac7b50af34cb36d7882019fa9
Emir Al <mythia@windowslive.com>
2022-04-11 15:37:31 +0300

    add: shapeTeamData func to shape team data

---
+++ src/utils/api/shapeTeamData.js
@@ -0,0 +1,25 @@
+import countryCodes from '../countryCodes';
+import teamInfo from '../teamInfo';
+
+export const shapeTeamData = (data) => {
+  const constructorData = data.MRData.ConstructorTable.Constructors;
+  const constructors = constructorData.map((team) => {
+    const { constructorId: id } = team;
+    return {
+      id,
+      name:
+        team.name === 'Alpine F1 Team'
+          ? 'Alpine'
+          : team.name === 'Haas F1 Team'
+          ? 'Haas'
+          : team.name,
+      countryCode: countryCodes(team.nationality, 'nationality'),
+      fullName: teamInfo[id].fullName,
+      base: teamInfo[id].base,
+      teamChief: teamInfo[id].teamChief,
+      chassis: teamInfo[id].chassis,
+      powerUnit: teamInfo[id].powerUnit,
+    };
+  });
+  return constructors;
+};
