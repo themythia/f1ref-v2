71ea3498a0b0d9a429f16f7af0d0b68efde84b7d
Emir Al <mythia@windowslive.com>
2022-04-11 11:43:10 +0300

    refactor: implement data fetching with new useFetch hook

--- src/components/DriversPage/DriversPage.jsx
+++ src/components/DriversPage/DriversPage.jsx
@@ -2,22 +2,28 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addDrivers } from '../../slices/driversSlice';
 import { getDrivers } from '../../utils/api';
+import { shapeDriverData } from '../../utils/api/shapeDriverData';
+import useFetch from '../../utils/useFetch';
 import DriverItem from './DriverItem';
 import DriverItemSkeleton from './DriverItemSkeleton';
 
 const DriversPage = () => {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const drivers = useSelector((store) => store.drivers);
 
+  const { loading, status, response, error } = useFetch(
+    'https://ergast.com/api/f1/current/drivers.json',
+    [],
+    shapeDriverData
+  );
+
+  console.log('drivers:', drivers);
+  console.log('loading:', loading);
+
   useEffect(() => {
-    if (drivers.length === 0) {
-      getDrivers().then((data) => {
-        dispatch(addDrivers(data));
-        setLoading(false);
-      });
-    } else if (drivers.length > 0) setLoading(false);
-  }, [drivers, dispatch]);
+    if (response) dispatch(addDrivers(response));
+  }, [dispatch, response]);
 
   const range = (min, max) =>
     Array.from({ length: max - min + 1 }, (_, i) => min + i);
