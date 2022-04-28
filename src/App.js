02d2ab0da1a26d3ad82e8dacef2d20947a75e2a7
Emir Al <mythia@windowslive.com>
2022-04-28 16:00:48 +0300

    remove: suspense from App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoadingSpinner from './components/shared/LoadingSpinner';
-
-const Navbar = React.lazy(() => import('./components/Navbar/Navbar'));
-const ThemeWrapper = React.lazy(() =>
-  import('./components/shared/ThemeWrapper')
-);
-const HomePage = React.lazy(() => import('./components/HomePage/HomePage'));
-const Footer = React.lazy(() => import('./components/Footer/Footer'));
-const CalendarPage = React.lazy(() =>
-  import('./components/CalendarPage/CalendarPage')
-);
-const DriversPage = React.lazy(() =>
-  import('./components/DriversPage/DriversPage')
-);
-const TeamsPage = React.lazy(() => import('./components/TeamsPage/TeamsPage'));
-const RacePage = React.lazy(() => import('./components/RacePage/RacePage'));
-const DriverPage = React.lazy(() =>
-  import('./components/DriverPage/DriverPage')
-);
-const TeamPage = React.lazy(() => import('./components/TeamPage/TeamPage'));
-const FourOhFour = React.lazy(() => import('./components/shared/FourOhFour'));
+import Navbar from './components/Navbar/Navbar';
+import ThemeWrapper from './components/shared/ThemeWrapper';
+import HomePage from './components/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
+import CalendarPage from './components/CalendarPage/CalendarPage';
+import DriversPage from './components/DriversPage/DriversPage';
+import TeamsPage from './components/TeamsPage/TeamsPage';
+import RacePage from './components/RacePage/RacePage';
+import DriverPage from './components/DriverPage/DriverPage';
+import TeamPage from './components/TeamPage/TeamPage';
+import FourOhFour from './components/shared/FourOhFour';
 
 function App() {
   //todo
@@ -28,30 +19,22 @@ function App() {
   // - readme
 
   return (
-    <Suspense
-      fallback={
-        <div className='h-screen w-screen flex flex-row justify-center items-center'>
-          <LoadingSpinner />
-        </div>
-      }
-    >
-      <ThemeWrapper>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='calendar' element={<CalendarPage />} />
-            <Route path='calendar/:round' element={<RacePage />} />
-            <Route path='drivers' element={<DriversPage />} />
-            <Route path='drivers/:driverId' element={<DriverPage />} />
-            <Route path='teams' element={<TeamsPage />} />
-            <Route path='teams/:teamId' element={<TeamPage />} />
-            <Route path='*' element={<FourOhFour />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </ThemeWrapper>
-    </Suspense>
+    <ThemeWrapper>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='calendar' element={<CalendarPage />} />
+          <Route path='calendar/:round' element={<RacePage />} />
+          <Route path='drivers' element={<DriversPage />} />
+          <Route path='drivers/:driverId' element={<DriverPage />} />
+          <Route path='teams' element={<TeamsPage />} />
+          <Route path='teams/:teamId' element={<TeamPage />} />
+          <Route path='*' element={<FourOhFour />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeWrapper>
   );
 }
 
