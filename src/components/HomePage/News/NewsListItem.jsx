9a99715f23389b1680ab1f0492fa5fc5e5799d00
Emir Al <mythia@windowslive.com>
2022-05-04 14:14:30 +0300

    fix: height of image not filling the container on sm screens

--- src/components/HomePage/News/NewsListItem.jsx
+++ src/components/HomePage/News/NewsListItem.jsx
@@ -6,14 +6,14 @@ const NewsListItem = ({ news }) => {
       href={link}
       target='_blank'
       rel='noreferrer'
-      className={`sm:hidden rounded bg-bg-200 dark:bg-bg-900 shadow-2px dark:shadow-2px-dark relative`}
+      className={`sm:hidden rounded bg-bg-200 dark:bg-bg-900 shadow-2px dark:shadow-2px-dark relative min-h-[76px]`}
     >
-      <div className='flex flex-row items-center w-full rounded shadow-2px dark:shadow-2px-dark bg-bg-200 dark:bg-bg-900 sm:hidden'>
-        <div className='basis-2/5'>
+      <div className='flex flex-row items-center w-full rounded shadow-2px dark:shadow-2px-dark bg-bg-200 dark:bg-bg-900 sm:hidden h-full'>
+        <div className='basis-2/5 min-h-[76px] overflow-hidden'>
           <img
             src={image}
             alt={description}
-            className='rounded-l shadow-2px dark:shadow-2px-dark'
+            className='rounded-l shadow-2px dark:shadow-2px-dark min-h-[76px]'
           />
         </div>
         <div className='basis-3/5 font-openSans font-semibold text-sm p-2 max-h-full h-full self-start'>
