dafd50ac71499b9189a04940a70e0ebbdca4be0c
Emir Al <mythia@windowslive.com>
2022-05-06 11:49:18 +0300

    update: 404 page

--- src/components/shared/FourOhFour.jsx
+++ src/components/shared/FourOhFour.jsx
@@ -1,12 +1,14 @@
+import { IoWarningOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+
 const FourOhFour = () => {
   return (
     <main className='p-4 sm:p-8 md:p-6 lg:px-[200px] xl:px-[calc((100vw-1128px)/2)] row-start-2 row-end-3 animate__animated animate__fadeIn'>
-      <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px dark:shadow-2px-dark p-4 md:p-6 w-full flex flex-col justify-center items-center h-full'>
-        <p className='font-poppins text-3xl mb-4 md:mb-6'>404</p>
-        <p className='font-openSans text-xl'>Page not found</p>
-        <p className='font-openSans text-sm'>
-          Please check the URL in the address bar and try again.
-        </p>
+      <div className='bg-bg-50 dark:bg-bg-800 rounded shadow-2px dark:shadow-2px-dark p-4 md:p-6 w-full flex flex-col justify-center items-center h-full gap-y-2 md:gap-y-3'>
+        <IoWarningOutline size={40} />
+        <p className='font-poppins text-3xl '>404</p>
+        <p className='font-openSans text-xl '>Page not found</p>
+        <Link to='/'></Link>
       </div>
     </main>
   );
